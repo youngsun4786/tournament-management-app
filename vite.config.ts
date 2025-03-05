4ba4c3991c73a6caff76f63ce99d95412a632a3f
Youngsun Yoo <youngsun123q@gmail.com>
2025-03-06 00:43:38 +0900

    integrate tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
