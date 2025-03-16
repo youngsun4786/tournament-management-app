c7eb0737b20f117991aec55d11dd8257ebfb3571
Youngsun Yoo <youngsun123q@gmail.com>
2025-03-17 04:01:10 +0900

    move container

--- app/container.ts
+++ app/container.ts
@@ -1,20 +1,22 @@
-import { GameService, IGameService } from './domains/services/game.service';
-import { IPlayerService, PlayerService } from './domains/services/player.service';
-import { ITeamService, TeamService } from './domains/services/team.service';
+import { GameService } from './domains/services/game.service';
+import { PlayerService } from './domains/services/player.service';
+import { TeamService } from './domains/services/team.service';
 
-let gameService: IGameService;
-let playerService: IPlayerService;
-let teamService: ITeamService;
+// let gameService: IGameService
+// let playerService: IPlayerService;
+// let teamService: ITeamService;
 
 
-if (process.env.NODE_ENV === 'testing') {
-  gameService = new GameService();
-  playerService = new PlayerService();
-  teamService = new TeamService();
-} else {
-    gameService = new GameService();
-    playerService = new PlayerService();
-    teamService = new TeamService();
-}
+// if (process.env.NODE_ENV === 'testing') {
+//   gameService = new GameService();
+//   playerService = new PlayerService();
+//   teamService = new TeamService();
+// } else {
+
+// }
+
+const gameService = new GameService();
+const playerService = new PlayerService();
+const teamService = new TeamService();
 
 export { gameService, playerService, teamService };
